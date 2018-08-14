3274559522d01d45c99e2d03f4cb07fc006b79b7
Roshan Louhar <rolouhar@microsoft.com>
2018-08-14 12:48:58 +0530

    sql queries added

--- IMP-SQLQueries-MYSQL.sql
+++ IMP-SQLQueries-MYSQL.sql
@@ -1,2 +1,11 @@
-select case when null = null then 'Yup' else 'Nope' end as Result;
+#select case when null = null then 'Yup' else 'Nope' end as Result;
+#select sysdate();
+#select YEAR(sysdate());
+#select MONTH(sysdate());
+#select DAY(sysdate());
+#SELECT hour(sysdate()) 'Hour Part' ;
+#SELECT minute(sysdate()) 'Minute Part';
+#SELECT second(sysdate()) 'Second Part';
+
+
 
